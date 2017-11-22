e8e74990ea06faddfcadb4a5604150298247f667
Veremey <veremey4uk@gmail.com>
2017-11-22 15:59:37 +0200

    velo

--- markup/components/head/data/data.js
+++
@@ -1,6 +0,0 @@
-head: {
-    defaults: {
-        title: 'slider',
-        useSocialMetaTags: false
-    }
-}
---
+++ static/js/common.js
@@ -0,0 +1,52 @@
+
+$(document).ready(function () {
+
+	var plotLineChart = document.querySelector('#line-chart');
+	var on = true;
+	var off = false;
+
+	$('.picture__curtain').addClass('is-load');
+
+	setTimeout(function () {
+		$(' .picture__img').removeClass('is-active');
+		$(' .picture_on').addClass('is-active');
+		$('.toggler__checkbox').addClass('is-active');
+	}, 3000);
+	setTimeout(function () {
+		$('.picture__curtain-duo').addClass('is-load');
+	}, 3200);
+
+	$(document).ready(function () {
+	/*-----toggler__desc click -------*/
+	/*---------------------*/
+	$('.toggler__desc').on('click', function () {
+
+		if ($(this).hasClass('toggler_on')) {
+			$(this).parents('.toggler').find('.toggler__checkbox').addClass('is-active');
+			$('.picture__img').removeClass('is-active');
+			$('.picture_on').addClass('is-active');
+		} else {
+			$(this).parents('.toggler').find('.toggler__checkbox').removeClass('is-active');
+			$('.picture__img').removeClass('is-active');
+			$('.picture_off').addClass('is-active');
+		}
+	});
+	/*-----toggler__btn click -------*/
+	/*---------------------*/
+	$('.toggler__btn').on('click', function (e) {
+		e.preventDefault();
+		var $toggler = $(this).parents('.toggler').find('.toggler__checkbox');
+
+		if ($toggler.hasClass('is-active')) {
+			$toggler.removeClass('is-active');
+			$('.picture__img').removeClass('is-active');
+			$('.picture_off').addClass('is-active');
+		} else {
+			$toggler.addClass('is-active');
+			$('.picture__img').removeClass('is-active');
+			$('.picture_on').addClass('is-active');
+		}
+	});
+});
+
+});
